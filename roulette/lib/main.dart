b073d1bd5f001f08fcd7efd06c60bdfa4783231d
91o-dev <91o.reality@gmail.com>
2024-10-19 05:16:42 +0900

    仮実装

---
+++ roulette/lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // 画面に表示する要素のインデックス番号を格納する用
+  int index = 0;
+  // ルーレットの起動有無フラグ
+  bool isStart = false;
+  // Timerオブジェクトを格納する用
+  var timer;
+  // ルーレットに選択肢として追加した要素を格納する用
+  List<String> elem = [];
+  // 要素にチェックが入っているかをboolで格納しておく用
+  List<bool> checkBox = [];
+  // チェックボックスで選択されている要素を格納する用
+  List<String> checkedElem = [];
+  // 画面上部に表示する要素を格納する用
+  String displayWord = 'Roulette';
+  // テキストフィールドにアクセスするためのコントローラー
+  TextEditingController addController = TextEditingController();
+
+  void startTimer() {
+    if (elem.length > 0 && checkedElem.length > 1) {
+      isStart = !isStart;
+      if (isStart) {
+        timer = Timer.periodic(Duration(milliseconds: 100), onTimer);
+      } else {
+        setState(() {
+          timer.cancel();
+        });
+      }
+    }
+  }
+
+  void onTimer(Timer timer) {
+    setState(() {
+      index++;
+      if (index > checkedElem.length - 1) {
+        index = 0;
+      }
+      displayWord = checkedElem[index];
+    });
+  }
+
+  void addElem() {
+    if (addController.text != '' && !isStart) {
+      setState(() {
+        elem.add(addController.text);
+        checkBox.add(true);
+        checkedElem.add(addController.text);
+        addController.text = '';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    displayWord,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Icon(
+                    Icons.playlist_add_outlined,
+                    color: Colors.blue,
+                    size: 40.0,
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: TextField(
+                    controller: addController,
+                    decoration: InputDecoration(
+                      hintText: 'input elem',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      addElem();
+                    },
+                    child: Text('Add'),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                width: double.infinity,
+                color: Colors.blue[100],
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: ListView.builder(
+                          itemCount: checkBox.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return CheckboxListTile(
+                              value: checkBox[index],
+                              title: Text(
+                                elem[index],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              controlAffinity: ListTileControlAffinity.leading,
+                              onChanged: (val) {
+                                if (!isStart) {
+                                  setState(() {
+                                    checkBox[index] = val!;
+                                    if (val) {
+                                      checkedElem.add(elem[index]);
+                                    } else {
+                                      checkedElem.remove(elem[index]);
+                                    }
+                                    // チェックした選択肢を追加、削除した際にはRangeErrorを回避するために一旦結果表示をリセット
+                                    displayWord = 'Roulette';
+                                  });
+                                }
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: isStart == true
+            ? Icon(
+                Icons.whatshot,
+                color: Colors.pink,
+              )
+            : Icon(Icons.whatshot),
+        onPressed: () {
+          startTimer();
+        },
+      ),
+    );
+  }
+}
